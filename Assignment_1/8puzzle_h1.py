4f945e2198b9e3fac06953825dbdf41f5628c1e5
Met <memetrusidovski@gmail.com>
2022-10-13 15:26:18 -0400

    cleaned

--- Assignment_1/8puzzle_h1.py
+++ Assignment_1/8puzzle_h1.py
@@ -27,61 +27,12 @@ z = Puzzle8.Puzzle(shuffle=False)
 
 
 x = y
+
 x.puzzle = [3,6,2,5,0,7,4,1,8]
 x.distCheck()
 x.findIndex()
 
 explored.append(x.puzzle)
-"""
-while x._dist != 0 and cost < 20:
-    up = deepcopy(x)
-    down = deepcopy(x)
-    left = deepcopy(x)
-    right = deepcopy(x)
-
-
-    x1 = up.up()
-    x2 = down.down()
-    x3 = left.left()
-    x4 = right.right()
-
-    up._globalCost = cost
-    down._globalCost = cost
-    left._globalCost = cost
-    right._globalCost = cost
-
-    if x1 and up not in explored:
-        q.put(up)
-        explored.append(up)
-        up.parent_node = x
-    if x2 and down not in explored:
-        q.put(down)
-        explored.append(down)
-        down.parent_node = x
-    if x3 and left not in explored:
-        q.put(left)
-        explored.append(left)
-        left.parent_node = x
-    if x4 and right not in explored:
-        q.put(right)
-        explored.append(right)
-        right.parent_node = x
-
-    x = q.get()
-    print(x._dist, " -------", x._globalCost)
-    cost += 1
-
-
-if(z in explored):
-    print(x, " <><><><>  \n\n", x._globalCost)
-
-while not q.empty():
-    next_item = q.get()
-    print(next_item._dist + next_item._globalCost, "\n~~~~~~~~~\n")
-
-
-"""
-x._globalCost += 1
 
 while x._dist != 0 and cost < 20000:
     up = deepcopy(x)
@@ -124,77 +75,17 @@ while x._dist != 0 and cost < 20000:
 
     x = q.get()
     x._globalCost += 1 
-    print(x)
-    print(x._dist, " -------", x._globalCost)
+    #print(x)
+    #print(x._dist, " -------", x._globalCost)
     cost += 1
 
 
+temp = x
+lst = []
+while temp.parent_node != None:
+    lst.append(temp)
+    temp = temp.parent_node
 
-
+for i in lst:
+    print(i)
 print(x)
-"""
-nodes = [x,y,z]
-
-print(x<z)
-nodes.sort()
-
-for i in nodes:
-    print(i._dist)
-
-
-q.put(x)
-q.put(y)
-q.put(z)
-
-while not q.empty():
-    next_item = q.get()
-    print(next_item._dist)
-
-first try, didnt work
-def search(puz, prev, cost): 
-    up = deepcopy(puz)
-    down = deepcopy(puz)
-    left = deepcopy(puz)
-    right = deepcopy(puz)
-
-    x1 = up.up()
-    x2 = down.down()
-    x3 = left.left()
-    x4 = right.right()
-
-    
-    m = [up if x1 else z,  down if x2 else z,
-         left if x3 else z,  right if x3 else z]
-    
-
-    #Swap sort to bring the best move to the front
-    if m[3]._dist < m[2]._dist and m[3].puzzle != prev.puzzle:
-        m[3], m[2] = m[2], m[3]
-
-    if m[2]._dist < m[1]._dist and m[2].puzzle != prev.puzzle:
-        m[2], m[1] = m[1], m[2]
-
-    if m[1]._dist < m[0]._dist and m[1].puzzle != prev.puzzle:
-        m[1], m[0] = m[0], m[1]
-    
-    if m[0] == prev:
-        return True
-        
-    
-
-    for i in m: 
-        print(i)
-    print("\n\n")
-
-  
-    if m[0]._dist != 0:
-        search(m[0], puz)
-    else:
-        return True
-    
-
-
-
-search(y, z, 0)
-    """
-
