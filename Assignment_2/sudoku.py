f6644c29e812f7559525c7ebcd0ab5cf010ef889
Met <memetrusidovski@gmail.com>
2022-11-02 17:12:31 -0400

    sudoku

---
+++ Assignment_2/sudoku.py
@@ -0,0 +1,5 @@
+import numpy as np
+import math
+from copy import copy, deepcopy
+from queue import PriorityQueue
+
--- Assignment_2/test.py
+++ Assignment_2/test.py
@@ -1,7 +1,11 @@
 from heapq import heappush
+from threading import Thread
 
 h = []
 a = heappush(h,  [2, 7, 1, 13, 14, 5, 15, 10, 11, 3, 0, 4, 12, 9, 6, 8])
 
+def copy():
+    
 print(h)
 print(a)
+
