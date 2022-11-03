beaeea75520aced8176febf931dd0abcbc95b595
Met <memetrusidovski@gmail.com>
2022-11-03 19:07:26 -0400

    board

--- Assignment_2/test.py
+++ Assignment_2/test.py
@@ -1,11 +1,5 @@
 from heapq import heappush
 from threading import Thread
 
-h = []
-a = heappush(h,  [2, 7, 1, 13, 14, 5, 15, 10, 11, 3, 0, 4, 12, 9, 6, 8])
 
-def copy():
-    
-print(h)
-print(a)
 
