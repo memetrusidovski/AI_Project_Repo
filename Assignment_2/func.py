cc8b30d2e25feec6a8cf743e9097731aa3db9303
Met <memetrusidovski@gmail.com>
2022-11-04 11:44:31 -0400

    stuff

---
+++ Assignment_2/func.py
@@ -0,0 +1,13 @@
+def print_board(b):
+    for i in range(len(b)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(b[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(b[i][j])
+            else:
+                print(str(b[i][j]) + " ", end="")
